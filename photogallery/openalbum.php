87d0549102d8776ecc9f554f1f6470cf7166f38f
Ravikant <ravikantpandey88@gmail.com>
2019-10-01 12:07:10 +0530

    Update openalbum.php

--- photogallery/openalbum.php
+++ photogallery/openalbum.php
@@ -17,7 +17,7 @@ $_SESSION["albumname"] = $_GET["albumname"];
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
+    <title>Album Info</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center; }
